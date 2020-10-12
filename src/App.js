7c9d5f238c8489af5e37f5097707052d5a73be2f
Nikita Shulga <nikita.shulga@gmail.com>
2020-10-12 09:42:40 -0700

    Update pytorch-nightly link to include binary jobs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => (
       <ul className="menu">
         <li>New-style (warning, does NOT show Jenkins builds):</li>
         <li><Link to="/build2/pytorch-master">pytorch-master</Link></li>
-        <li><Link to="/build2/pytorch-nightly">pytorch-nightly</Link></li>
+        <li><Link to="/build2/pytorch-nightly?mode=nightly">pytorch-nightly</Link></li>
         <li><Link to="/build2/pytorch-release/1.7">pytorch-release/1.7</Link></li>
       </ul>
       <ul className="deprecated-menu">
