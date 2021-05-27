8f2b887fbd8f668d9eeb7e76eeb7f78ae2ce37cf
Jon Janzen <jonjanzen@fb.com>
2021-05-27 09:27:25 -0700

    Update repo link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => (
   <Router basename={process.env.PUBLIC_URL + '/'}>
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title"><Link to="/">ci.pytorch.org HUD</Link> (<a href="https://github.com/ezyang/pytorch-ci-hud">GitHub</a>)</h1>
+        <h1 className="App-title"><Link to="/">ci.pytorch.org HUD</Link> (<a href="https://github.com/pytorch/pytorch-ci-hud">GitHub</a>)</h1>
       </header>
       <ul className="menu">
         <li>New-style (warning, does NOT show Jenkins builds):</li>
