21c265ea860be028d800798a4acd208893036dc9
Sam Estep <sestep@fb.com>
2021-06-07 12:44:57 -0700

    Mention LICENSE in README and in source files

--- src/JobCorrelationHeatmap.js
+++ src/JobCorrelationHeatmap.js
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import React, { Component } from "react";
 import "@toast-ui/chart/dist/toastui-chart.css";
 import Chart from "@toast-ui/chart";
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
