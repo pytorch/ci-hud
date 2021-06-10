driazati/pytorch-ci-hud

Add a per-PR view
This is inspired by https://prow.k8s.io/, which provides an external page for information about PRs. We can add basically whatever we want here which frees us from being locked into GitHub comments (e.g. with Dr. CI). For now it's pretty basic and just shows artifacts (both on GitHub and in S3) for GitHub Action jobs.

There's also a bit to get authenticated with GitHub via OAuth, which from what I can tell requires something like https://github.com/prose/gatekeeper. I spun it up at metrics.pytorch.org for now (we can get auth.pytorch.org provisioned as a more permament home), a PR to test-infra for the deploy scripts is incoming.

src/App.js
@@ -10,8 +10,10 @@ import QueueDisplay from "./QueueDisplay.js";
 import BuildHistoryDisplay from "./BuildHistoryDisplay.js";
 import GitHubStatusDisplay from "./GitHubStatusDisplay.js";
 import PerfHistoryDisplay from "./PerfHistoryDisplay.js";
+import PrDisplay from "./PrDisplay.js";
 import JobCorrelationHeatmap from "./JobCorrelationHeatmap.js";
 import GitHubActionsDisplay from "./GitHubActionsDisplay.js";
+import AuthorizeGitHub from "./GithubAuthorize.js";
 import {
   BrowserRouter as Router,
   Route,
@@ -96,8 +98,11 @@ const App = () => (
       <Switch>
         <Route path="/build" component={BuildRoute} />
         <Route path="/build1" component={Build1Route} />
+        <Route path="/pr" component={PrRoute} />
         <Route path="/build2" component={Build2Route} />
         <Route path="/torchbench-v0-nightly" component={TorchBenchRoute} />
+        <Route path="/github_logout" component={LogoutGitHub} />
+        <Route path="/authorize_github" component={AuthorizeGithubRoute} />
         <Route path="/status" component={Status} />
         <Route exact path="/">
           <Redirect to="/build2/pytorch-master" />
@@ -132,6 +137,16 @@ const Status = () => (
     <ComputerDisplay interval={1000} />
   </div>
 );
+
+const AuthorizeGithubRoute = () => {
+  return <AuthorizeGitHub />;
+};
+
+const LogoutGitHub = () => {
+  localStorage.removeItem("gh_pat");
+  console.log("logged out");
+  return <Redirect to="/"></Redirect>;
+};
 
 const Build = ({ match }) => {
   // Uhhh, am I really supposed to rob window.location here?
@@ -168,6 +183,17 @@ const Build2 = ({ match }) => {
     />
   );
 };
+
+const PrPage = ({ match }) => {
+  return <PrDisplay pr_number={parseInt(match.url.replace(/^\/pr\//, ""))} />;
+};
+
+const PrRoute = ({ match }) => (
+  <Fragment>
+    <Route exact path={match.url} component={PrPage} />
+    <Route path={`${match.url}/:segment`} component={PrRoute} />
+  </Fragment>
+);
 
 const BuildRoute = ({ match }) => (
   <Fragment>

src/PrDisplay.js
@@ -1,0 +1,281 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthorizeGitHub from "./GithubAuthorize.js";
+
+import { parseXml, formatBytes, asyncAll, s3, github } from "./utils.js";
+
+function getPrQuery(number) {
+  return `
+    {
+      repository(name: "pytorch", owner: "pytorch") {
+        pullRequest(number: ${number}) {
+          title
+          number
+          url
+          commits(last: 1) {
+            nodes {
+              commit {
+                checkSuites(last: 100) {
+                  nodes {
+                    databaseId
+                    workflowRun {
+                      runNumber
+                      id
+                      databaseId
+                      workflow {
+                        name
+                      }
+                      url
+                    }
+                    checkRuns(last: 100) {
+                      nodes {
+                        name
+                        title
+                        text
+                        detailsUrl
+                        summary
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+}
+
+export default class PrDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pr_number: this.props.pr_number,
+    };
+  }
+  componentDidMount() {
+    this.update();
+  }
+
+  extractItem(result, key) {
+    // Some of the stuff from s3 can come in as a single object or an array,
+    // so unpack that here
+    if (!result[key]) {
+      return null;
+    }
+
+    if (Array.isArray(result[key])) {
+      return result[key];
+    }
+    return [result[key]];
+  }
+
+  async update() {
+    // Fetch the PR's info from GitHub's GraphQL API
+    localStorage.getItem("gh_pat");
+    if (!localStorage.getItem("gh_pat")) {
+      return;
+    }
+    let pr_result = await github.graphql(getPrQuery(this.state.pr_number));
+    this.state.pr = pr_result.repository.pullRequest;
+
+    // The GraphQL API doesn't have any types for artifacts (at least as far as
+    // I can tell), so we have to fall back to iterating through them all via
+    // the v3 JSON API
+    let workflow_runs = this.state.pr.commits.nodes[0].commit.checkSuites.nodes;
+    workflow_runs = workflow_runs.filter((x) => x.workflowRun);
+    await asyncAll(
+      workflow_runs.map((run) => {
+        return async () => {
+          let id = run.workflowRun.databaseId;
+          run.artifacts = await github.json(
+            `repos/pytorch/pytorch/actions/runs/${id}/artifacts`
+          );
+        };
+      })
+    );
+
+    this.state.runs = workflow_runs;
+    this.setState(this.state);
+
+    // Go through all the runs and check if there is a prefix for the workflow
+    // run in S3 (indicating that there are some relevant artifacts stored
+    // there)
+    let promises = this.state.runs.map((run) => {
+      run.s3_artifacts = [];
+      return async () => {
+        // Check that the workflow run exists
+        let result = await s3(`pytorch/pytorch/${run.workflowRun.databaseId}/`);
+
+        let prefixes = this.extractItem(
+          result.ListBucketResult,
+          "CommonPrefixes"
+        );
+
+        // If anything was found, go through the results and add the items to
+        // the 'run' object in place
+        if (prefixes && prefixes.length > 0) {
+          for (const prefixItem of prefixes) {
+            let prefix = prefixItem.Prefix["#text"];
+            let result = await s3(prefix);
+            let contents = this.extractItem(
+              result.ListBucketResult,
+              "Contents"
+            );
+            for (const content of contents) {
+              run.s3_artifacts.push(content);
+            }
+          }
+        }
+      };
+    });
+    await asyncAll(promises);
+    this.setState(this.state);
+  }
+
+  render() {
+    let runs = undefined;
+    if (this.state.runs) {
+      runs = [];
+
+      // Render all of the check runs as a
+      for (const [run_index, run] of this.state.runs.entries()) {
+        const checks = [];
+        for (const [index, check] of run.checkRuns.nodes.entries()) {
+          checks.push(
+            <div key={"check-run-" + index}>
+              - <a href={check.detailsUrl}>{check.name}</a>
+            </div>
+          );
+        }
+
+        let artifacts = [];
+        function makeArtifact(args) {
+          if (args.expired) {
+            return (
+              <div key={`${args.kind}-${args.index}`}>
+                <span>
+                  [{args.kind}] {args.name}
+                </span>{" "}
+                <span>({formatBytes(args.size_in_bytes)}) (expired)</span>
+              </div>
+            );
+          } else {
+            return (
+              <div key={`${args.kind}-${args.index}`}>
+                <a href={args.url}>
+                  [{args.kind}] {args.name}
+                </a>{" "}
+                <span>({formatBytes(args.size_in_bytes)})</span>
+              </div>
+            );
+          }
+        }
+
+        // List out artifacts hosted on GitHub
+        if (run.artifacts) {
+          for (const [index, artifact] of run.artifacts.artifacts.entries()) {
+            // The URL in the is for the API, not browsers, so make it manually
+            let url = `https://github.com/pytorch/pytorch/suites/${run.databaseId}/artifacts/${artifact.id}`;
+            artifacts.push(
+              makeArtifact({
+                kind: "gha",
+                index: index,
+                name: artifact.name,
+                size_in_bytes: artifact.size_in_bytes,
+                url: url,
+                expired: artifact.expired,
+              })
+            );
+          }
+        }
+
+        // List out artifacts from s3
+        if (run.s3_artifacts) {
+          for (const [index, artifact] of run.s3_artifacts.entries()) {
+            let prefix = artifact.Key["#text"];
+
+            artifacts.push(
+              makeArtifact({
+                kind: "s3",
+                index: index,
+                name: prefix.split("/").slice(-1),
+                size_in_bytes: parseInt(artifact.Size["#text"]),
+                url: `https://gha-artifacts.s3.amazonaws.com/${prefix}`,
+                expired: false,
+              })
+            );
+          }
+        }
+
+        // If there were any artifacts, set up the 'div' to show them
+        let artifactsElement = <div></div>;
+        if (artifacts.length > 0) {
+          artifactsElement = (
+            <div style={{ padding: "6px" }}>
+              <h5>Aritifacts</h5>
+              {artifacts}
+            </div>
+          );
+        }
+
+        let checksElement = <div></div>;
+        if (checks.length > 0) {
+          checksElement = (
+            <div style={{ padding: "6px" }}>
+              <h5>Checks</h5>
+              {checks}
+            </div>
+          );
+        }
+
+        // Wrap up everything in a card
+        const card = (
+          <Card key={"card-" + run_index}>
+            <Card.Body>
+              <Card.Title>
+                <a href={run.workflowRun.url}>
+                  {run.workflowRun.workflow.name}
+                </a>
+              </Card.Title>
+              <div>
+                {checksElement}
+                {artifactsElement}
+              </div>
+            </Card.Body>
+          </Card>
+        );
+        runs.push(card);
+      }
+    }
+
+    return (
+      <div>
+        <AuthorizeGitHub />
+
+        <h2>
+          <a
+            href={
+              "https://github.com/pytorch/pytorch/pull/" + this.state.pr_number
+            }
+          >
+            PR #{this.state.pr_number}
+          </a>
+        </h2>
+        <p>
+          {this.state.pr
+            ? this.state.pr.title
+            : "loading (make sure you are signed in)..."}
+        </p>
+        <div>{runs}</div>
+      </div>
+    );
+  }
+}

pytorch/ci-hud

src/App.js
@@ -10,8 +10,10 @@ import QueueDisplay from "./QueueDisplay.js";
 import BuildHistoryDisplay from "./BuildHistoryDisplay.js";
 import GitHubStatusDisplay from "./GitHubStatusDisplay.js";
 import PerfHistoryDisplay from "./PerfHistoryDisplay.js";
+import PrDisplay from "./PrDisplay.js";
 import JobCorrelationHeatmap from "./JobCorrelationHeatmap.js";
 import GitHubActionsDisplay from "./GitHubActionsDisplay.js";
+import AuthorizeGitHub from "./AuthorizeGitHub.js";
 import {
   BrowserRouter as Router,
   Route,
@@ -96,8 +98,11 @@ const App = () => (
       <Switch>
         <Route path="/build" component={BuildRoute} />
         <Route path="/build1" component={Build1Route} />
+        <Route path="/pr" component={PrRoute} />
         <Route path="/build2" component={Build2Route} />
         <Route path="/torchbench-v0-nightly" component={TorchBenchRoute} />
+        <Route path="/github_logout" component={LogoutGitHub} />
+        <Route path="/authorize_github" component={AuthorizeGithubRoute} />
         <Route path="/status" component={Status} />
         <Route exact path="/">
           <Redirect to="/build2/pytorch-master" />
@@ -132,6 +137,16 @@ const Status = () => (
     <ComputerDisplay interval={1000} />
   </div>
 );
+
+const AuthorizeGithubRoute = () => {
+  return <AuthorizeGitHub />;
+};
+
+const LogoutGitHub = () => {
+  localStorage.removeItem("gh_pat");
+  console.log("logged out");
+  return <Redirect to="/"></Redirect>;
+};
 
 const Build = ({ match }) => {
   // Uhhh, am I really supposed to rob window.location here?
@@ -168,6 +183,17 @@ const Build2 = ({ match }) => {
     />
   );
 };
+
+const PrPage = ({ match }) => {
+  return <PrDisplay pr_number={parseInt(match.url.replace(/^\/pr\//, ""))} />;
+};
+
+const PrRoute = ({ match }) => (
+  <Fragment>
+    <Route exact path={match.url} component={PrPage} />
+    <Route path={`${match.url}/:segment`} component={PrRoute} />
+  </Fragment>
+);
 
 const BuildRoute = ({ match }) => (
   <Fragment>

src/PrDisplay.js
@@ -1,0 +1,286 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthorizeGitHub from "./AuthorizeGitHub.js";
+
+import { parseXml, formatBytes, asyncAll, s3, github } from "./utils.js";
+
+function getPrQuery(number) {
+  return `
+    {
+      repository(name: "pytorch", owner: "pytorch") {
+        pullRequest(number: ${number}) {
+          title
+          number
+          url
+          commits(last: 1) {
+            nodes {
+              commit {
+                checkSuites(last: 100) {
+                  nodes {
+                    databaseId
+                    workflowRun {
+                      runNumber
+                      id
+                      databaseId
+                      workflow {
+                        name
+                      }
+                      url
+                    }
+                    checkRuns(last: 100) {
+                      nodes {
+                        name
+                        title
+                        text
+                        detailsUrl
+                        summary
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+}
+
+export default class PrDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pr_number: this.props.pr_number,
+    };
+  }
+  componentDidMount() {
+    this.update();
+  }
+
+  extractItem(result, key) {
+    // Some of the stuff from s3 can come in as a single object or an array,
+    // so unpack that here
+    if (!result[key]) {
+      return null;
+    }
+
+    if (Array.isArray(result[key])) {
+      return result[key];
+    }
+    return [result[key]];
+  }
+
+  async update() {
+    // Set some global persistent state to redirect back to this window for log
+    // ins
+    localStorage.setItem("last_redirect", window.location.href);
+
+    // Fetch the PR's info from GitHub's GraphQL API
+    localStorage.getItem("gh_pat");
+    if (!localStorage.getItem("gh_pat")) {
+      return;
+    }
+    let pr_result = await github.graphql(getPrQuery(this.state.pr_number));
+    this.state.pr = pr_result.repository.pullRequest;
+
+    // The GraphQL API doesn't have any types for artifacts (at least as far as
+    // I can tell), so we have to fall back to iterating through them all via
+    // the v3 JSON API
+    let workflow_runs = this.state.pr.commits.nodes[0].commit.checkSuites.nodes;
+    workflow_runs = workflow_runs.filter((x) => x.workflowRun);
+    await asyncAll(
+      workflow_runs.map((run) => {
+        return async () => {
+          let id = run.workflowRun.databaseId;
+          run.artifacts = await github.json(
+            `repos/pytorch/pytorch/actions/runs/${id}/artifacts`
+          );
+        };
+      })
+    );
+
+    this.state.runs = workflow_runs;
+    this.setState(this.state);
+
+    // Go through all the runs and check if there is a prefix for the workflow
+    // run in S3 (indicating that there are some relevant artifacts stored
+    // there)
+    let promises = this.state.runs.map((run) => {
+      run.s3_artifacts = [];
+      return async () => {
+        // Check that the workflow run exists
+        let result = await s3(`pytorch/pytorch/${run.workflowRun.databaseId}/`);
+
+        let prefixes = this.extractItem(
+          result.ListBucketResult,
+          "CommonPrefixes"
+        );
+
+        // If anything was found, go through the results and add the items to
+        // the 'run' object in place
+        if (prefixes && prefixes.length > 0) {
+          for (const prefixItem of prefixes) {
+            let prefix = prefixItem.Prefix["#text"];
+            let result = await s3(prefix);
+            let contents = this.extractItem(
+              result.ListBucketResult,
+              "Contents"
+            );
+            for (const content of contents) {
+              run.s3_artifacts.push(content);
+            }
+          }
+        }
+      };
+    });
+    await asyncAll(promises);
+    this.setState(this.state);
+  }
+
+  render() {
+    let runs = undefined;
+    if (this.state.runs) {
+      runs = [];
+
+      // Render all of the check runs as a list
+      for (const [run_index, run] of this.state.runs.entries()) {
+        const checks = [];
+        for (const [index, check] of run.checkRuns.nodes.entries()) {
+          checks.push(
+            <div key={"check-run-" + index}>
+              - <a href={check.detailsUrl}>{check.name}</a>
+            </div>
+          );
+        }
+
+        let artifacts = [];
+        function makeArtifact(args) {
+          if (args.expired) {
+            return (
+              <div key={`${args.kind}-${args.index}`}>
+                <span>
+                  [{args.kind}] {args.name}
+                </span>{" "}
+                <span>({formatBytes(args.size_in_bytes)}) (expired)</span>
+              </div>
+            );
+          } else {
+            return (
+              <div key={`${args.kind}-${args.index}`}>
+                <a href={args.url}>
+                  [{args.kind}] {args.name}
+                </a>{" "}
+                <span>({formatBytes(args.size_in_bytes)})</span>
+              </div>
+            );
+          }
+        }
+
+        // List out artifacts hosted on GitHub
+        if (run.artifacts) {
+          for (const [index, artifact] of run.artifacts.artifacts.entries()) {
+            // The URL in the response is for the API, not browsers, so make it
+            // manually
+            let url = `https://github.com/pytorch/pytorch/suites/${run.databaseId}/artifacts/${artifact.id}`;
+            artifacts.push(
+              makeArtifact({
+                kind: "gha",
+                index: index,
+                name: artifact.name,
+                size_in_bytes: artifact.size_in_bytes,
+                url: url,
+                expired: artifact.expired,
+              })
+            );
+          }
+        }
+
+        // List out artifacts from s3
+        if (run.s3_artifacts) {
+          for (const [index, artifact] of run.s3_artifacts.entries()) {
+            let prefix = artifact.Key["#text"];
+
+            artifacts.push(
+              makeArtifact({
+                kind: "s3",
+                index: index,
+                name: prefix.split("/").slice(-1),
+                size_in_bytes: parseInt(artifact.Size["#text"]),
+                url: `https://gha-artifacts.s3.amazonaws.com/${prefix}`,
+                expired: false,
+              })
+            );
+          }
+        }
+
+        // If there were any artifacts, set up the 'div' to show them
+        let artifactsElement = <div></div>;
+        if (artifacts.length > 0) {
+          artifactsElement = (
+            <div style={{ padding: "6px" }}>
+              <h5>Artifacts</h5>
+              {artifacts}
+            </div>
+          );
+        }
+
+        let checksElement = <div></div>;
+        if (checks.length > 0) {
+          checksElement = (
+            <div style={{ padding: "6px" }}>
+              <h5>Checks</h5>
+              {checks}
+            </div>
+          );
+        }
+
+        // Wrap up everything in a card
+        const card = (
+          <Card key={"card-" + run_index}>
+            <Card.Body>
+              <Card.Title>
+                <a href={run.workflowRun.url}>
+                  {run.workflowRun.workflow.name}
+                </a>
+              </Card.Title>
+              <div>
+                {checksElement}
+                {artifactsElement}
+              </div>
+            </Card.Body>
+          </Card>
+        );
+        runs.push(card);
+      }
+    }
+
+    return (
+      <div>
+        <AuthorizeGitHub />
+
+        <h2>
+          <a
+            href={
+              "https://github.com/pytorch/pytorch/pull/" + this.state.pr_number
+            }
+          >
+            PR #{this.state.pr_number}
+          </a>
+        </h2>
+        <p>
+          {this.state.pr
+            ? this.state.pr.title
+            : "loading (make sure you are signed in)..."}
+        </p>
+        <div>{runs}</div>
+      </div>
+    );
+  }
+}