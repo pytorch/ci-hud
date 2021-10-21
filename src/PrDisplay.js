95aa24e35f83b1f6db003520d92f4a82d1de18f9
driazati <9407960+driazati@users.noreply.github.com>
2021-10-21 16:24:49 -0700

    Support CircleCI on PR Page (#165)
    This adds CircleCI builds alongside GitHub Action jobs. CircleCI doesn't let you link to individual steps, just jobs. This is just basic support, some features are missing but they are pretty low priority since we'd just be re-implementing Circle's views of these which are pretty decent:
    * artifacts
    * test results
    
    Co-authored-by: David Riazati <driazati@users.noreply.github.com>

--- src/PrDisplay.js
+++ src/PrDisplay.js
@@ -12,6 +12,8 @@ import AuthorizeGitHub from "./AuthorizeGitHub.js";
 import TestReportRenderer from "./pr/TestReportRenderer.js";
 import Editor from "@monaco-editor/react";
 import ReactMarkdown from "react-markdown";
+import { filterLog, registerLogLanguage } from "./pr/logs.js";
+import CircleCICard from "./pr/CircleCICard.js";
 
 import {
   BsFillCaretRightFill,
@@ -55,6 +57,14 @@ function getPrQuery(user, repo, number) {
           commits(last: 1) {
             nodes {
               commit {
+                status {
+                  contexts {
+                    description
+                    context
+                    targetUrl
+                    state
+                  }
+                }
                 checkSuites(last: 100) {
                   nodes {
                     databaseId
@@ -103,6 +113,14 @@ function getCommitQuery(user, repo, hash) {
                 commitUrl
                 messageHeadline
                 messageBody
+                status {
+                  contexts {
+                    description
+                    context
+                    targetUrl
+                    state
+                  }
+                }
                 checkSuites(last: 100) {
                   nodes {
                     databaseId
@@ -279,6 +297,7 @@ export default class PrDisplay extends Component {
         run.checkRuns.nodes.map((check) => check.conclusion)
       );
     }
+    this.state.statuses = this.state.commit.status.contexts;
     this.setState(this.state);
 
     // Go through all the runs and check if there is a prefix for the workflow
@@ -491,121 +510,6 @@ export default class PrDisplay extends Component {
     );
   }
 
-  filterLog(log) {
-    let negativeRegexes = [
-      /Z Entering 'third_party/,
-      /Z http\.https:\/\/github\.com\/\.extraheader/,
-      /Z deleted: sha256/,
-      /Z untagged: sha256/,
-      /Z untagged: .*amazonaws/,
-      /Z \s*adding: /,
-      /Z \s*creating: /,
-      /Z \s*inflating: /,
-      /Z \s*extracting: /,
-      /Z adding /,
-      /Z copying /,
-      /Z creating /,
-      /Z refs\/remotes\/origin/,
-      /Z Synchronizing submodule url for/,
-      /Z Receiving objects:/,
-      /Z Resolving deltas:/,
-      /Z remote: Compressing objects:/,
-      /Z Submodule path /,
-      /Z remote: Counting objects:/,
-      /Z [a-z0-9]{12}: Waiting/,
-      /Z [a-z0-9]{12}: Pulling fs layer/,
-      /Z [a-z0-9]{12}: Verifying Checksum/,
-      /Z [a-z0-9]{12}: Download complete/,
-      /Z [a-z0-9]{12}: Pull complete/,
-      /Z url\.https:\/\/github\.com/,
-      /Z Generating XML reports/,
-      /Z Generated XML report/,
-      /Z Test results will be stored/,
-    ];
-
-    const lines = log.split("\n");
-    let newLog = "";
-
-    for (const line of lines) {
-      let include = true;
-      for (const regex of negativeRegexes) {
-        if (line.match(regex)) {
-          include = false;
-          break;
-        }
-      }
-      if (include) {
-        newLog += line + "\n";
-      }
-    }
-    return newLog;
-  }
-
-  registerLogLanguage(monaco, groupRanges) {
-    // Register a new language
-    monaco.languages.register({ id: "logText" });
-
-    // Register a tokens provider for the language
-    monaco.languages.setMonarchTokensProvider("logText", {
-      tokenizer: {
-        root: [
-          [/\[30;1m.*?\[0m/, "black"],
-          [/\[31;1m.*?\[0m/, "red"],
-          [/\[32;1m.*?\[0m/, "green"],
-          [/\[33;1m.*?\[0m/, "yellow"],
-          [/\[34;1m.*?\[0m/, "blue"],
-          [/\[35;1m.*?\[0m/, "magenta"],
-          [/\[36;1m.*?\[0m/, "cyan"],
-          [/\[37;1m.*?\[0m/, "white"],
-
-          [/\[30m.*?\[0m/, "black"],
-          [/\[31m.*?\[0m/, "red"],
-          [/\[32m.*?\[0m/, "green"],
-          [/\[33m.*?\[0m/, "yellow"],
-          [/\[34m.*?\[0m/, "blue"],
-          [/\[35m.*?\[0m/, "magenta"],
-          [/\[36m.*?\[0m/, "cyan"],
-          [/\[37m.*?\[0m/, "white"],
-
-          [/\[0;1;30m.*?\[0m/, "black"],
-          [/\[0;1;31m.*?\[0m/, "red"],
-          [/\[0;1;32m.*?\[0m/, "green"],
-          [/\[0;1;33m.*?\[0m/, "yellow"],
-          [/\[0;1;34m.*?\[0m/, "blue"],
-          [/\[0;1;35m.*?\[0m/, "magenta"],
-          [/\[0;1;36m.*?\[0m/, "cyan"],
-          [/\[0;1;37m.*?\[0m/, "white"],
-
-          [/\[1m.*?\[0m/, "cyan"],
-          [/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{7}Z/, "hidden"],
-        ],
-      },
-    });
-
-    // Define a new theme that contains only rules that match this language
-    monaco.editor.defineTheme("logTheme", {
-      base: "vs-dark",
-      inherit: true,
-      rules: [
-        { token: "black", foreground: "000000", fontStyle: "bold" },
-        { token: "red", foreground: "af0000", fontStyle: "bold" },
-        { token: "green", foreground: "87d700", fontStyle: "bold" },
-        { token: "yellow", foreground: "d7d700", fontStyle: "bold" },
-        { token: "blue", foreground: "5fd7ff", fontStyle: "bold" },
-        { token: "magenta", foreground: "8700af", fontStyle: "bold" },
-        { token: "cyan", foreground: "00af87", fontStyle: "bold" },
-        { token: "white", foreground: "ffffff", fontStyle: "bold" },
-        { token: "hidden", foreground: "5f5f5f" },
-      ],
-    });
-
-    monaco.languages.registerFoldingRangeProvider("logText", {
-      provideFoldingRanges: function (model, context, token) {
-        return groupRanges;
-      },
-    });
-  }
-
   getGroupRanges(monaco, text) {
     const lines = text.split("\n");
     const starts = [];
@@ -641,7 +545,7 @@ export default class PrDisplay extends Component {
         if (check.log.logLevel == "All") {
           logText = check.log.text;
         } else {
-          logText = this.filterLog(check.log.text);
+          logText = filterLog(check.log.text);
         }
 
         const totalLines = (logText.match(/\n/g) || "").length + 1;
@@ -688,7 +592,7 @@ export default class PrDisplay extends Component {
               theme="logTheme"
               beforeMount={(monaco) => {
                 const groupRanges = this.getGroupRanges(monaco, logText);
-                this.registerLogLanguage(monaco, groupRanges);
+                registerLogLanguage(monaco, groupRanges);
               }}
               options={{
                 scrollBeyondLastLine: false,
@@ -1089,10 +993,25 @@ export default class PrDisplay extends Component {
       loading = <p>Loading... (make sure you are signed in)</p>;
     }
 
+    if (this.state.statuses) {
+      for (const status of this.state.statuses) {
+        if (!status.targetUrl.includes("https://circleci.com")) {
+          continue;
+        }
+        runs.push({
+          data: {
+            status: status.state,
+          },
+          element: <CircleCICard key={status.context} status={status} />,
+        });
+      }
+    }
+
     let displayRuns = [];
     function add(type) {
       for (const run of runs) {
         if (run.data.status === type) {
+          run.used = true;
           displayRuns.push(
             <div key={run.data.databaseId} style={{ marginBottom: "4px" }}>
               {run.element}
@@ -1106,6 +1025,12 @@ export default class PrDisplay extends Component {
     add("SUCCESS");
     add("GROUP");
     add("SKIPPED");
+
+    for (const run of runs) {
+      if (!run.used) {
+        console.error("Unused run with state", run.data.status);
+      }
+    }
 
     if (this.state.runs && displayRuns.length == 0) {
       displayRuns = <p style={{ fontWeight: "bold" }}>No jobs found</p>;
